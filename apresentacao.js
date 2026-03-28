04b3b95c2e2320ddc9c87bddf0c6e4ff64ba3ded
HenriqueSilvaXavier <rodrigoxgamer2@gmail.com>
2026-03-28 18:59:45 -0300

    Novo currículo

--- apresentacao.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var menuHamburguer = document.getElementById('menu-hamburguer');
-    var menu = document.querySelector('.menu-lista');
-
-    menuHamburguer.addEventListener('click', function () {
-        menu.classList.toggle('visivel');
-    });
-});
